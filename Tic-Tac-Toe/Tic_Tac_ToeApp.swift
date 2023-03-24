980577eda72377da7ecf9e812a409b5a9fa066b8
vorontsov <mimictear@outlook.com>
2023-03-24 15:17:58 +0100

    Initial Commit

---
+++ Tic-Tac-Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic-Tac-Toe
+//
+//  Created by ANDREY VORONTSOV on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
